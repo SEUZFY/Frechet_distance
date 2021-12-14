faf339cb2ba361ae22bace2d73d254deb917485f
zmocheng <zmocheng@gmail.com>
2021-12-14 12:28:01 +0100

    Add code in CurveSimilarities.py. Read from two csv files and compute the Fréchet Distance of two curves.

--- src/CurveSimilarities.py
+++ src/CurveSimilarities.py
@@ -1,6 +1,33 @@
+import numpy as np
+import pandas as pd
 import Frechet
 
 if __name__ == "__main__":
-    P = [(1,1),(2,2)]
-    Q = [(1,1),(2,2),(4,4)]
-    print(Frechet.frechet_distance(P,Q)) 
+
+    # open file
+    file_object = open(r"D:\MicrosoftVisualStudio\myprojects\Frechet_distance\test_files\loc1_each_timestamp.csv", encoding='UTF-8')
+    try:
+        file_content = file_object.readlines() # not very huge file, use readlines() to speed up
+    finally:
+        file_object.close()
+    # curve_a
+    splitor = ','
+    curve_a = []
+    for each in file_content:
+        curve_a.append((float(each.split(splitor)[0]), float(each.split(splitor)[1])))
+
+
+    # open file
+    file_object = open(r"D:\MicrosoftVisualStudio\myprojects\Frechet_distance\test_files\loc2_each_timestamp.csv", encoding='UTF-8')
+    try:
+        file_content = file_object.readlines() # not very huge file, use readlines() to speed up
+    finally:
+        file_object.close()
+    # curve_b
+    splitor = ','
+    curve_b = []
+    for each in file_content:
+        curve_b.append((float(each.split(splitor)[0]), float(each.split(splitor)[1])))
+
+    # calculate Frechet distance
+    print(Frechet.frechet_distance(curve_a,curve_b)) 
